80d5d5433837ce1c34fc665dcef7cb1f424d11ac
Georgios Zacharias <97185439+GeorgiosZacharias@users.noreply.github.com>
2026-04-06 14:20:49 +0300

    UPDATED

--- 1DSORT.cbl
+++ 1DSORT.cbl
@@ -14,8 +14,9 @@
       *-----------------------
        WORKING-STORAGE SECTION.
        01  MYAREA              PIC X(20) VALUE '57663311442205748396'.
-       01  ONE-TABLE.
-           05  TABLE-POS OCCURS 10 TIMES  PIC 99 .
+       01  ONE-TABLE REDEFINES MYAREA.
+           05  TABLE-POS   OCCURS 10 TIMES.
+               10 TABLE-DIGIT PIC 99.
        01  IDI                           PIC 9(2).
        01  IDJ                           PIC 9(2).
        01  TABLE-SUM                     PIC 9(4) VALUE 0.
@@ -28,7 +29,6 @@
        PROCEDURE DIVISION.
       *
        MAIN-PROCEDURE.
-           INITIALIZE ONE-TABLE.
            PERFORM PROCESS-TABLE.
            PERFORM CALCULATIONS-TABLE.
            PERFORM SORT-TABLE.
@@ -39,21 +39,19 @@
            DISPLAY '***DISPLAY INTERNAL TABLE CONTENTS ***'
            DISPLAY '------------------------------------------------'
            PERFORM VARYING IDI FROM 1 BY 1 UNTIL IDI>10
-               MOVE MYAREA(TEMP:2) TO TABLE-POS(IDI)
-               ADD 2 TO TEMP
-               DISPLAY 'ENTRY' IDI '-->'  TABLE-POS(IDI)
+               DISPLAY 'ENTRY' IDI '-->'  TABLE-DIGIT(IDI)
            END-PERFORM.
        CALCULATIONS-TABLE.
            DISPLAY '------------------------------------------------'
            DISPLAY '***FIND AVERANGE, MIN & MAX NUMBERS OF ITABLE***'
            DISPLAY '------------------------------------------------'
            PERFORM VARYING IDI FROM 1 BY 1 UNTIL IDI>10
-               ADD TABLE-POS(IDI) TO TABLE-SUM
-               IF  TABLE-POS(IDI)>TABLE-MAX
-                   MOVE TABLE-POS(IDI) TO TABLE-MAX
+               ADD TABLE-DIGIT(IDI) TO TABLE-SUM
+               IF  TABLE-DIGIT(IDI)>TABLE-MAX
+                   MOVE TABLE-DIGIT(IDI) TO TABLE-MAX
                END-IF
-               IF  TABLE-POS(IDI)<TABLE-MIN
-                   MOVE TABLE-POS(IDI) TO TABLE-MIN
+               IF  TABLE-DIGIT(IDI)<TABLE-MIN
+                   MOVE TABLE-DIGIT(IDI) TO TABLE-MIN
                END-IF
            END-PERFORM
            COMPUTE TABLE-AVG = TABLE-SUM / (IDI - 1)
@@ -63,10 +61,10 @@
        SORT-TABLE.
            PERFORM VARYING IDI FROM 1 BY 1 UNTIL IDI > 9
             PERFORM VARYING IDJ FROM IDI BY 1 UNTIL IDJ > 10
-                IF TABLE-POS(IDI) > TABLE-POS(IDJ)
-                    MOVE TABLE-POS(IDI) TO TEMP-SORT
-                    MOVE TABLE-POS(IDJ) TO TABLE-POS(IDI)
-                    MOVE TEMP-SORT TO TABLE-POS(IDJ)
+                IF TABLE-DIGIT(IDI) > TABLE-DIGIT(IDJ)
+                    MOVE TABLE-DIGIT(IDI) TO TEMP-SORT
+                    MOVE TABLE-DIGIT(IDJ) TO TABLE-DIGIT(IDI)
+                    MOVE TEMP-SORT TO TABLE-DIGIT(IDJ)
                 END-IF
             END-PERFORM
            END-PERFORM.
@@ -77,7 +75,7 @@
            DISPLAY '------------------------------------------------'
 
            PERFORM VARYING IDI FROM 1 BY 1 UNTIL IDI>10
-               DISPLAY 'ENTRY' IDI '-->' TABLE-POS(IDI)
+               DISPLAY 'ENTRY' IDI '-->' TABLE-DIGIT(IDI)
            END-PERFORM.
        FINISH.
            STOP RUN.
